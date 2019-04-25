b4fe0ada813ea3105e17f51f4b42013b57e501d4
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-04-25 16:34:33 -0500

    Implementing calculator function for sum of common multiples

--- problem1/problem1.cpp
+++ problem1/problem1.cpp
@@ -14,9 +14,35 @@ int sum_of_multiples(int find_multiples_of, int upper_limit)
     return sum;
 }
 
+int sum_of_common_multiples(int a, int b, int upper_limit)
+{
+    int larger_integer = (a > b) ? (a) : (b);
+    int multiple_of_a = 0;
+    int multiple_of_b = 0;
+    int sum = 0;
+    int multiplier = 1;
+
+    while(multiplier * larger_integer < upper_limit)
+    {
+        multiple_of_a = a * multiplier;
+        multiple_of_b = b * multiplier;
+
+        if (multiple_of_a == multiple_of_b)
+        {
+            sum += multiple_of_a;
+        }
+
+        ++multiplier;
+    }
+
+    return sum;
+}
+
 int main()
 {
-    int upper_limit = 1000;
-    std::cout << "The sum of all multiples of 3 or 5 below " << upper_limit << " is " << sum_of_multiples(3, upper_limit) + sum_of_multiples(5, upper_limit) << std::endl;
+    int upper_limit = 16;
+    std::cout << "The sum of all multiples of 3 or 5 below " << upper_limit << " is " 
+              << (sum_of_multiples(3, upper_limit) + sum_of_multiples(5, upper_limit) - sum_of_common_multiples(3, 5, upper_limit))
+              << std::endl;
     return 0;
 }
