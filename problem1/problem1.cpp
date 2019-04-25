f246e05c2124ab97f840a97467fb6b28300a04c8
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-04-25 14:23:07 -0500

    Rectifiy algorithm does not add the same multiple two times

--- problem1/problem1.cpp
+++ problem1/problem1.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 
-// This function finds the sum of all multiples of the integer "find_multiples_of".
+// This function finds the sum of all multiples of the integers "a" or "b".
 // All found multiples should be below the integer "upper_limit".
-int sum_of_multiples(int find_multiples_of, int upper_limit)
+int sum_of_multiples(int a, int b, int upper_limit)
 {
+    int larger_integer = (a > b) ? (a) : (b);
+    int multiple_of_a = 0;
+    int multiple_of_b = 0;
     int sum = 0;
     int multiplier = 1;
-    while(multiplier * find_multiples_of < upper_limit)
+
+    while(multiplier * larger_integer < upper_limit)
     {
-        sum += multiplier * find_multiples_of;
+        multiple_of_a = a * multiplier;
+        multiple_of_b = b * multiplier;
+
+        if (multiple_of_a != multiple_of_b)
+        {
+            sum += (multiple_of_a + multiple_of_b);
+        }
+        else
+        {
+            sum += multiple_of_a;
+        }
+
         ++multiplier;
     }
+
     return sum;
 }
 
 int main()
 {
     int upper_limit = 1000;
-    std::cout << "The sum of all multiples of 3 or 5 below " << upper_limit << " is " << sum_of_multiples(3, upper_limit) + sum_of_multiples(5, upper_limit) << std::endl;
+    std::cout << "The sum of all multiples of 3 or 5 below " << upper_limit << " is " << sum_of_multiples(3, 5, upper_limit) << std::endl;
     return 0;
 }
