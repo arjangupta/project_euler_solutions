0a4a5c4b3051f7cb6d2f73aea94728320523a570
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-04-25 14:11:38 -0500

    Write algorithm for finding sum of multiples

--- problem1/problem1.cpp
+++ problem1/problem1.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 
-int sum_of_multiples(int multiples_of, int upper_limit)
+// This function finds the sum of all multiples of the integer "find_multiples_of".
+// All found multiples should be below the integer "upper_limit".
+int sum_of_multiples(int find_multiples_of, int upper_limit)
 {
-    return 0;
+    int sum = 0;
+    int multiplier = 1;
+    while(multiplier * find_multiples_of < upper_limit)
+    {
+        sum += multiplier * find_multiples_of;
+        ++multiplier;
+    }
+    return sum;
 }
 
 int main()
 {
     int upper_limit = 1000;
-    std::cout << "The sum of the multiples of 3 and 5 below " << upper_limit << " is " << sum_of_multiples(3, upper_limit) + sum_of_multiples(5, upper_limit) << std::endl;
+    std::cout << "The sum of all multiples of 3 or 5 below " << upper_limit << " is " << sum_of_multiples(3, upper_limit) + sum_of_multiples(5, upper_limit) << std::endl;
     return 0;
 }
