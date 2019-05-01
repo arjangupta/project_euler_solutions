22f8436866bf105240bbbe9c2090163b6d3cda43
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-05-01 14:10:23 -0500

    Implement the sum of common multiples function in Python

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -11,4 +11,18 @@ def sum_of_multiples(integer, upper_limit):
         multiple = integer * multiplier
     return sum
 
-print(f"Sum of the multiples of 5 below 16 is {sum_of_multiples(5, 16)}")
+def sum_of_common_multiples(integer1, integer2, upper_limit):
+    multiplier = 1
+    sum = 0
+    larger_integer  = integer1 if integer1 > integer2 else integer2
+    smaller_integer = integer1 if integer1 < integer2 else integer2
+    multiple = larger_integer * multiplier
+    while multiple < upper_limit:
+        if multiple % smaller_integer == 0:
+            sum += multiple
+        multiplier += 1
+        multiple = larger_integer * multiplier
+    return sum
+
+given_upper_limit = 1000
+print(f"Sum of the multiples of 3 or 5 below 1000 is {sum_of_multiples(3, given_upper_limit) + sum_of_multiples(5, given_upper_limit) - sum_of_common_multiples(3, 5, given_upper_limit)}")
